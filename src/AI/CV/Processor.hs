3a4dfd041b7c7e8383794e979eddc2c696310b8f
Noam <noam@duapov.(none)>
2010-03-10 20:58:33 +0200

    Before changing to deep-dsl

--- src/AI/CV/Processor.hs
+++ src/AI/CV/Processor.hs
@@ -1,8 +1,9 @@
 {-# LANGUAGE RankNTypes #-}
 
 module AI.CV.Processor 
-    ( processor,
+    ( simpleProcessor,
       Processor,
+      SimpleProcessor,
       empty,
       chain,
       run
@@ -10,22 +11,26 @@ module AI.CV.Processor
 
 
 
+type SimpleProcessor m a b o = Processor m a b () o
+
 -- The idea is that the monad m is usually IO, and that a and b are usually pointers.
 -- It is meant for functions that require a pre-allocated output pointer to operate.
-type ProcessFunc m a b c = a -> b -> m c 
-type AllocateNextFunc m a b = a -> m b 
-type ReleaseNextFunc m b = b -> m ()
-data Processor m a b c = Processor' { process :: ProcessFunc m a b c
-                                 , allocateNext :: AllocateNextFunc m a b
-                                 , releaseNext :: ReleaseNextFunc m b
-                                 }
+data Processor m a b x o = Processor { process :: a -> (b,x) -> m o
+                                     , allocateNext :: a -> m (b,x) 
+                                     , releaseNext :: (b,x) -> m ()
+                                     }
 
-processor :: Monad m => 
-             ProcessFunc m a b c -> 
-             AllocateNextFunc m a b ->   
-             ReleaseNextFunc m b ->
-             Processor m a b c
-processor pf af rf = Processor' pf af rf
+simpleProcessor :: Monad m => 
+             (a -> b -> m o) -> 
+             (a -> m b)      ->   
+             (b -> m ())     ->
+             SimpleProcessor m a b o
+simpleProcessor pf af rf = Processor pf' af' rf'
+    where pf' a (b,_) = pf a b
+          af' a = do 
+            b <- af a
+            return (b,())
+          rf' (b,_) = rf b
 
 --type Source a c = Processor () a c
 --type Sink a c   = Processor a () c
@@ -33,30 +38,31 @@ processor pf af rf = Processor' pf af rf
 doNothing :: Monad m => m ()
 doNothing = do return ()
                
-empty :: Monad m => Processor m a () ()
-empty = Processor' { process = const (const doNothing)
-                   , allocateNext = const doNothing 
-                   , releaseNext = const doNothing }
+empty :: Monad m => SimpleProcessor m a () ()
+empty = simpleProcessor pf af rf
+    where pf = const (const doNothing)
+          af = const doNothing 
+          rf = const doNothing 
 
 
-chainProcess :: (Monad m) => Processor m a b d -> Processor m b c e -> a -> (b,c) -> m e
-chainProcess p1 p2 a (b,c) = do
-  process p1 a b
-  process p2 b c
+chainProcess :: (Monad m) => Processor m a b x o -> Processor m b c x' o' -> a -> (c,((b,x),x')) -> m o'
+chainProcess p1 p2 a (c,((b,x),x')) = do
+  process p1 a (b,x)
+  process p2 b (c,x')
 
 -- todo: any way to re-use the allocation that's being done in chainProcess?
-chainAllocateNext :: Monad m => Processor m a b d -> Processor m b c e -> a -> m (b,c)
-chainAllocateNext p1 p2 x = do
-  y <- allocateNext p1 x
-  z <- allocateNext p2 y
-  return (y,z)
+chainAllocateNext :: Monad m => Processor m a b x o -> Processor m b c x' o' -> a -> m (c,((b,x),x'))
+chainAllocateNext p1 p2 a = do
+  (b,x) <- allocateNext p1 a
+  (c,x') <- allocateNext p2 b
+  return (c,((b,x),x'))
   
-chainReleaseNext :: Monad m => Processor m a b d -> Processor m b c e -> (b,c) -> m ()
-chainReleaseNext p1 p2 (y,z) = do
-  releaseNext p2 z
-  releaseNext p1 y
+chainReleaseNext :: Monad m => Processor m a b x o -> Processor m b c x' o' -> (c,((b,x),x')) -> m ()
+chainReleaseNext p1 p2 (c,((b,x),x')) = do
+  releaseNext p2 (c,x')
+  releaseNext p1 (b,x)
 
-chain :: Monad m => Processor m a b d -> Processor m b c e -> Processor m a (b,c) e
+chain :: Monad m => Processor m a b x o -> Processor m b c x' o' -> Processor m a c ((b,x),x') o'
 chain p1 p2 = Processor' { process = chainProcess p1 p2
                          , allocateNext = chainAllocateNext p1 p2 
                          , releaseNext = chainReleaseNext p1 p2 
@@ -65,10 +71,10 @@ chain p1 p2 = Processor' { process = chainProcess p1 p2
 -- i'd like it to be a Monoid, but how? the 'm c' result of process and the allocation
 -- result 'm b' seem to prevent having any candidate for mempty.
 
-run :: Monad m => Processor m a b c -> a -> m c
-run p x = do
-        y <- allocateNext p x
-        z <- process p x y
-        releaseNext p y
-        return z
+run :: Monad m => Processor m a b x o -> a -> m o
+run p a = do
+        (b,x) <- allocateNext p a
+        o <- process p a (b,x)
+        releaseNext p (b,x)
+        return o
 
