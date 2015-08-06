c1b8bb0b7ae5c766a4cc53f254c5cec7f95937aa
PLR <plredmond@gmail.com>
2015-08-05 18:16:24 -0700

    bring up to date with latest version of HOpenCV on git, 0.3

--- src/AI/CV/ImageProcessors.hs
+++ src/AI/CV/ImageProcessors.hs
@@ -34,6 +34,7 @@ module AI.CV.ImageProcessors
      videoFile,
 
      window,
+     namedWindow,
 
      resize,
      dilate,
@@ -48,17 +49,15 @@ module AI.CV.ImageProcessors
 
 import Control.Processor(runUntil, IOSink, IOSource, IOProcessor, processor)
 
-import AI.CV.OpenCV.Types(PImage)
 import qualified AI.CV.OpenCV.CV as CV
+import AI.CV.OpenCV.CV (HaarClassifierCascade)
+
 import qualified AI.CV.OpenCV.CxCore as CxCore
-import AI.CV.OpenCV.CxCore(CvSize, CvRect, CvMemStorage)
+import AI.CV.OpenCV.CxCore (CvSize, CvRect, MemStorage)
+
 import qualified AI.CV.OpenCV.HighGui as HighGui
-import AI.CV.OpenCV.CV(CvHaarClassifierCascade)
 
-import Foreign.Ptr(Ptr)
-
-
-type Image = PImage
+type Image = CxCore.IplImage
 
 type ImageSink      = IOSink      Image
 type ImageSource    = IOSource    ()     Image
@@ -71,7 +70,7 @@ type ImageProcessor = IOProcessor Image  Image
 
 -- | Predicate for pressed keys
 keyPressed :: Show a => a -> IO Bool
-keyPressed _ = fmap (/= -1) $ HighGui.waitKey 3 -- todo wrap waitKey more generally for the API
+keyPressed _ = fmap (/= Nothing) $ HighGui.waitKey 3 -- todo wrap waitKey more generally for the API
 
 -- | Runs the processor until a predicate is true, for predicates, and processors that take () as input
 -- (such as chains that start with a camera).
@@ -83,52 +82,56 @@ runTillKeyPressed :: (Show a) => IOProcessor () a -> IO ()
 runTillKeyPressed f = f `runTill` keyPressed >> (return ())
 
 ------------------------------------------------------------------
-capture :: IO (Ptr HighGui.CvCapture) -> ImageSource
-capture pCap = processor processQueryFrame allocateCamera fromState releaseNext
-    where processQueryFrame :: () -> (Ptr CxCore.IplImage, Ptr HighGui.CvCapture) 
-                               -> IO (Ptr CxCore.IplImage, Ptr HighGui.CvCapture)
+capture :: IO HighGui.Capture -> ImageSource
+capture getCap = processor processQueryFrame allocateCamera fromState releaseNext
+    where processQueryFrame :: () -> (Image, HighGui.Capture) 
+                               -> IO (Image, HighGui.Capture)
           processQueryFrame _ (_, cap) = do
-            newFrame <- HighGui.cvQueryFrame cap
+            newFrame <- HighGui.queryFrame cap
             return (newFrame, cap)
           
-          allocateCamera :: () -> IO (Ptr CxCore.IplImage, Ptr HighGui.CvCapture)
+          allocateCamera :: () -> IO (Image, HighGui.Capture)
           allocateCamera _ = do
-            cap <- pCap
-            newFrame <- HighGui.cvQueryFrame cap
+            cap <- getCap
+            newFrame <- HighGui.queryFrame cap
             return (newFrame, cap)
           
           fromState (image, _) = return image
           
-          releaseNext (_, cap) = HighGui.cvReleaseCapture cap
+          releaseNext (_, cap) = HighGui.releaseCapture cap
 
 
 -- | A capture device, using OpenCV's HighGui lib's cvCreateCameraCapture
 -- should work with most webcames. See OpenCV's docs for information.
 -- This processor outputs the latest image from the camera at each invocation.
 camera :: Int -> ImageSource
-camera index = capture (HighGui.cvCreateCameraCapture (fromIntegral index))
+camera index = capture (HighGui.createCameraCapture (fromIntegral index))
 
 videoFile :: String -> ImageSource
-videoFile fileName = capture (HighGui.cvCreateFileCapture fileName)
+videoFile fileName = capture (HighGui.createFileCapture fileName)
 
 ------------------------------------------------------------------
 -- GUI stuff  
             
 -- | A window that displays images.
 -- Note: windows with the same index will be the same window....is this ok?
+-- Response: Yes. It will display whatever images you give it.
 window :: Int -> ImageSink
-window num = processor procFunc allocFunc return return
+window num = namedWindow (show num) True
+
+namedWindow :: String -> Bool -> ImageSink
+namedWindow s a = processor procFunc allocFunc return return
     where procFunc :: (Image -> () -> IO ())
-          procFunc src x = HighGui.showImage (fromIntegral num) src >> return x
+          procFunc src x = HighGui.showImage s src >> return x
           
           allocFunc :: (Image -> IO ())
-          allocFunc _ = HighGui.newWindow (fromIntegral num) True
+          allocFunc _ = HighGui.namedWindow s a
 
 ------------------------------------------------------------------
 -- | A convenience function for constructing a common type of processors that work exclusively on images
 imageProcessor :: (Image -> Image -> IO Image) -> (Image -> IO Image) 
                -> ImageProcessor
-imageProcessor procFunc allocFunc = processor procFunc allocFunc return CxCore.cvReleaseImage
+imageProcessor procFunc allocFunc = processor procFunc allocFunc return CxCore.releaseImage
 
 -- | OpenCV's cvResize
 resize :: Int -- Width
@@ -136,19 +139,19 @@ resize :: Int -- Width
        -> CV.InterpolationMethod -> ImageProcessor
 resize width height interp = imageProcessor processResize allocateResize
     where processResize src dst = do
-            CV.cvResize src dst interp
+            CV.resize src dst interp
             return dst
             
           allocateResize src = do
             nChans <- CxCore.getNumChannels src :: IO Int
             depth <- CxCore.getDepth src
-            CxCore.cvCreateImage (CxCore.CvSize (fromIntegral width) (fromIntegral height)) (fromIntegral nChans) depth
+            CxCore.createImage (CxCore.CvSize (fromIntegral width) (fromIntegral height)) depth (fromIntegral nChans)
           
 -- | OpenCV's cvDilate
 dilate :: Int -> ImageProcessor
-dilate iterations = imageProcessor procDilate CxCore.cvCloneImage
+dilate iterations = imageProcessor procDilate CxCore.cloneImage
     where procDilate src dst = do
-            CV.cvDilate src dst (fromIntegral iterations) 
+            CV.dilate src dst (fromIntegral iterations) 
             return dst
 
 
@@ -160,20 +163,21 @@ canny :: Int  -- ^ Threshold 1
          -> ImageProcessor
 canny thres1 thres2 size = processor processCanny allocateCanny convertState releaseState
     where processCanny src (gray, dst) = do
-            HighGui.cvConvertImage src gray 0 
-            CV.cvCanny gray dst (fromIntegral thres1) (fromIntegral thres2) (fromIntegral size)
+            HighGui.convertImage src gray 0 
+            CV.canny gray dst (fromIntegral thres1) (fromIntegral thres2) (fromIntegral size)
             return (gray, dst)
             
           allocateCanny src = do
-            target <- CxCore.cvCreateImage (CxCore.cvGetSize src) 1 CxCore.iplDepth8u
-            gray <- CxCore.cvCreateImage (CxCore.cvGetSize src) 1 CxCore.iplDepth8u
+            srcSize <- CxCore.getSize src
+            target <- CxCore.createImage srcSize CxCore.iplDepth8u 1
+            gray <- CxCore.createImage srcSize CxCore.iplDepth8u 1
             return (gray, target)
             
           convertState = return . snd
                             
           releaseState (gray, target) = do
-            CxCore.cvReleaseImage gray
-            CxCore.cvReleaseImage target
+            CxCore.releaseImage gray
+            CxCore.releaseImage target
 
 ------------------------------------------------------------------
 
@@ -185,24 +189,24 @@ haarDetect :: String  -- ^ Cascade filename (OpenCV comes with several, includin
            -> CvSize  -- ^ min size
            -> IOProcessor Image [CvRect]
 haarDetect cascadeFileName scaleFactor minNeighbors flags minSize = processor procFunc allocFunc convFunc freeFunc 
-    where procFunc :: Image -> ([CvRect], (Ptr CvHaarClassifierCascade, Ptr CvMemStorage)) 
-                   -> IO ([CvRect], (Ptr CvHaarClassifierCascade, Ptr CvMemStorage))
+    where procFunc :: Image -> ([CvRect], (HaarClassifierCascade, MemStorage)) 
+                   -> IO ([CvRect], (HaarClassifierCascade, MemStorage))
           procFunc image (_, x@(cascade, storage)) = do
-            seqP <- CV.cvHaarDetectObjects image cascade storage (realToFrac scaleFactor) (fromIntegral minNeighbors) flags minSize
+            seqP <- CV.haarDetectObjects image cascade storage (realToFrac scaleFactor) (fromIntegral minNeighbors) flags minSize
             recs <- CxCore.seqToList seqP
             return (recs, x)
             
-          allocFunc :: Image -> IO ([CvRect], (Ptr CvHaarClassifierCascade, Ptr CvMemStorage))
+          allocFunc :: Image -> IO ([CvRect], (HaarClassifierCascade, MemStorage))
           allocFunc _ = do
-            storage <- CxCore.cvCreateMemStorage 0
-            (cascade, name) <- CxCore.cvLoad cascadeFileName storage Nothing
+            storage <- CxCore.createMemStorage 0
+            (cascade, name) <- CxCore.load cascadeFileName storage Nothing
             print name -- todo verify that this is a haar cascade
             return ([], (cascade, storage))
           
           convFunc = return . fst
           
           freeFunc (_, (_, storage)) = do
-            CxCore.cvReleaseMemStorage storage
+            CxCore.releaseMemStorage storage
             -- todo release the cascade usign cvReleaseHaarClassifierCascade
           
             
@@ -213,10 +217,10 @@ haarDetect cascadeFileName scaleFactor minNeighbors flags minSize = processor pr
             
 -- | OpenCV's cvRectangle, currently without width, color or line type control
 drawRects :: IOProcessor (Image, [CvRect]) Image
-drawRects = processor procFunc (CxCore.cvCloneImage . fst) return CxCore.cvReleaseImage
+drawRects = processor procFunc (CxCore.cloneImage . fst) return CxCore.releaseImage
     where procFunc (src,rects) dst = do
-            CxCore.cvCopy src dst
-            mapM_ (CxCore.cvRectangle dst) rects
+            CxCore.copy src dst
+            mapM_ (CxCore.rectangle dst) rects
             return dst
             
 
